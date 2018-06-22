8a9127ed0ae18d563c67b6d6dd2041ece7c20f09
Nick Dugger <email@nickdugger.com>
2018-06-22 09:44:14 -0500

    Update index.js

--- libraries/std/index.js
+++ libraries/std/index.js
@@ -1,7 +1,23 @@
 const Library = require('../../core/Library');
 
+const position = require('./components/position');
+const texture = require('./components/texture');
+
+const player = require('./entities/player');
+
+const move = require('./systems/move');
+const render = require('./systems/render');
+
 module.exports = new Library('std', {
-    components: [ ],
-    entities: [ ],
-    systems: [ ]
+    components: [
+        position,
+        texture
+    ],
+    entities: [
+        player
+    ],
+    systems: [
+        move,
+        render
+    ]
 });
