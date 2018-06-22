8be23cd8a0254944742a8669edee32d9968db5ba
Nick Dugger <email@nickdugger.com>
2018-06-22 09:50:36 -0500

    Update Library.js

--- core/Library.js
+++ core/Library.js
@@ -1,3 +1,22 @@
 module.exports = class Library {
 
+    constructor (name, lib) {
+        this.kind = name;
+        
+        this.components = { };
+        this.entities = { };
+        this.systems = { };
+        
+        for (const component of lib.components) {
+            this.components[ component.kind ] = component;
+        }
+        
+        for (const entity of lib.entities) {
+            this.entities[ entity.kind ] = entity;
+        }
+        
+        for (const system of lib.systems) {
+            this.systems[ system.kind ] = system;
+        }
+    }
 };
